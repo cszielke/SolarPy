cd68eb0ac73fb246954194afd007f70b20aac94d
Claus Zielke <info@cszielke.de>
2019-12-23 12:39:13 +0100

    setup mit requirements.txt

--- setup.bat
+++ setup.bat
@@ -1,8 +1,1 @@
-pip3 install paho-mqtt
-pip3 install argparse
-pip3 install ConfigParser
-pip3 install pyserial
-pip3 install dataclasses
-rem pip3 install dataclasses-json
-pip3 install jsons
-pip3 install pymysql
+pip install -r requirements.txt
