ab4f391574161f3fb33369a4ba2cb93436747ed9
Claus Zielke <info@cszielke.de>
2020-01-12 16:38:26 +0100

    Webcam Video Scripte erweitert

--- scripts/encode_pictures.ps1
+++ scripts/encode_pictures.ps1
@@ -2,45 +2,95 @@
 # FFMpeg has to be installed and destignation dir must not contain JPG images. 
 # They will be erased without further questions
 
+$sourcedir=$args[0]
+$destvideodir=$args[1]
+
 $ffmpeg="V:\Videotools\ffmpeg\ffmpeg.exe"
 $bitrate=1500000
-$sourcedir=$args[0]
-$destdir="V:\Videotools\ffmpeg\tmp"
+$tmpdir="V:\Videotools\ffmpeg\tmp"
 
 if($null -eq $sourcedir){
     # Testcase"
-    $sourcedir = "C:\Users\zielke\Desktop\20200110" 
+    $sourcedir = "\\raspidbsrv\web\html\webcam\20200111" 
 }
 
+if($null -eq $destvideodir){
+    # Testcase"
+    $destvideodir="\\raspidbsrv\web\html\webcam\videos"
+}
+
+if(!(Test-Path $ffmpeg -PathType Leaf)){
+    Write-Output "FFMpeg expected to be installed in '$ffmpeg'"
+    exit 1
+}
+
+
+#Get the Date from file paths last chunk. (file date could be also possible)
 $date = Split-Path -Path $sourcedir -Resolve -Leaf
 Write-Output "Date: $date"
 
-if(!(Test-Path -Path $destdir)){
-    Write-Output "Create $destdir"
+# Check if $date is numeric
+if(!($date -match "^\d+$")){
+    Write-Output "Last chunk of $sourcedir is not numeric. Exiting."
+    exit 2
+}
 
-    New-Item -ItemType directory -Path $destdir
+$destvideofile="$destvideodir\pv$date.mp4"
+write-output "Check if Video file '$destvideofile' already exist."
+
+if(Test-Path $destvideofile -PathType Leaf){
+    Write-Output "'$destvideofile' exist."
+    exit 0
 }
 else{
-    Write-Output "Delete $destdir Images"
-    Get-ChildItem $destdir/*.jpg -Include * -Recurse | Remove-Item
+    write-output "Check Video output dir '$destvideodir'"
+    if(!(Test-Path -Path $destvideodir)){
+        Write-Output "Create '$destvideodir'"
+    
+        New-Item -ItemType directory -Path $destvideodir
+    }
+    else{
+        Write-Output "$destvideodir exist."
+    }
+
+    write-output "Check Tmp dir '$tmpdir'"
+    if(!(Test-Path -Path $tmpdir)){
+        Write-Output "Create Temp dir: '$tmpdir'"
+
+        New-Item -ItemType directory -Path $tmpdir
+    }
+    else{
+        Write-Output "Delete files in '$tmpdir' "
+        Get-ChildItem $tmpdir/* -Include * -Recurse | Remove-Item
+    }
+
+    Write-Output "Copy images from '$sourcedir' to '$tmpdir'"
+    Copy-Item -Path "$sourcedir\*.jpg" -Destination "$tmpdir" -Recurse
+
+    Write-Output "Rename files for numeric order"
+    $i = 0
+    Get-ChildItem "$tmpdir\*.jpg" | ForEach-Object {
+        $newfn = ('pv{0:D4}.jpg' -f $i++)
+        # Write-Output "Rename file $_ to $newfn"
+        Rename-Item $_ -NewName $newfn
+    }
+
+    Write-Output "'$destvideofile' not exist."
+
+    Write-Output "Start FFMpeg"
+    & $ffmpeg -r 12 -f image2 -i "$tmpdir\pv%04d.jpg" -b:v $bitrate "$destvideofile"
+    $success = $?
+    Write-Output ExitCode=$success
+    if($success){
+        Write-Output "FFMpeg Ok"
+        Write-Output "Removing temp files"
+        Get-ChildItem $tmpdir/* -Include * -Recurse | Remove-Item
+        exit 0
+    }
+    else{
+        Write-Output "FFMpeg Error!"
+        # Read-Host -Prompt "Press Enter to continue"
+        exit 1
+    }
 }
-
-Write-Output "Copy $sourcedir to $destdir"
-
-Copy-Item -Path "$sourcedir\*" -Destination "$destdir" -Recurse
-
-$i = 0
-Get-ChildItem "$destdir\*.jpg" | ForEach-Object {
-    Write-Output "Rename file $_ to $i"
-    Rename-Item $_ -NewName ('pv{0:D4}.jpg' -f $i++)
-}
-
-Write-Output "State FFMpeg"
-& $ffmpeg -r 12 -f image2 -i "$destdir\pv%04d.jpg" -b:v $bitrate "V:\Videotools\ffmpeg\tmp\pv$date.mp4"
-$success = $?
-Write-Output ExitCode=$success
-if($success){
-    Write-Output "Removing images"
-    Get-ChildItem $destdir/*.jpg -Include * -Recurse | Remove-Item
-}
-# Read-Host -Prompt "Press Enter to continue"
+exit 100  # schould not happen
--- scripts/makeallpvvideos.ps1
+++ scripts/makeallpvvideos.ps1
@@ -1,17 +1,46 @@
 # Make videos out of every Webcam directory
 
 $sourcedir=$args[0]
-$encodescript="V:\Videotools\ffmpeg\encode_pictures.ps1"
+$destvideodir=$args[1]
+
+$encodescript="encode_pictures.ps1"
+
+function PrintUsage {
+    Write-Output 'Usage: $script "<sourcedir>" "<video/destignation/dir>"'
+    Write-Output '    <sourcedir> must contain subdirectorys with the date as name in format "YYYYmmdd".'
+    Write-Output '    Example:'
+    Write-Output '    <sourcedir> +- 20191223 +-pv000237.jpg'
+    Write-Output '                |           +-pv000237.jpg'
+    Write-Output '                |           | ....'
+    Write-Output '                |           +-pv235924.jpg'
+    Write-Output '                +- 20191224'
+    Write-Output '                | ....'
+    Write-Output '                L- 20200112'
+}
 
 if($null -eq $sourcedir){
+    Write-Output 'Usage Error! No source dir!'
+    PrintUsage
+    exit 1
     # default dir"
-    $sourcedir = "\\192.168.15.160\c\SolarPy\templates\public\webcam" 
+    #$sourcedir = "\\raspidbsrv\web\html\webcam" 
 }
+
+if($null -eq $destvideodir){
+    Write-Output 'Usage Error! No destignation dir!'
+    PrintUsage
+    exit 1
+    # default dir"
+    # $destvideodir = "\\raspidbsrv\web\html\webcam\videos" 
+}
+
+$scriptdir = Split-Path $script:MyInvocation.MyCommand.Path
+Write-Output "Current script directory is $scriptdir"
 
 Get-ChildItem "$sourcedir\*" | ForEach-Object {
     if((Get-Item $_) -is [System.IO.DirectoryInfo]){
         Write-Output "Working on directory '$_'"
-        & $encodescript $_
+        & $scriptdir\$encodescript $_ $destvideodir
     }
     else{
         Write-Output "$_ is not a directory"
